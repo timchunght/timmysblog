089e149b8b73b78cac4c6a01b817458d3dddb8da
Timothy Chung <timchunght@gmail.com>
2015-11-02 19:18:20 -0500

    Added right domain for production

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://timchunght.me',
+        url: 'http://timchung.me',
         mail: {},
         database: {
             client: 'sqlite3',
